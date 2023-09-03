20f9ac1c4d5104924ba3aaa4ba762b239198fa73
furylx <101798731+furylx@users.noreply.github.com>
2023-09-03 14:20:16 +0200

    More argparsing and errorhandling

--- cmd/main.go
+++ cmd/main.go
@@ -6,19 +6,23 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
+
+	scn "goport/pkg/scanner"
 
 	"github.com/urfave/cli/v2"
 )
 
 func main() {
+	start := time.Now()
 	app := &cli.App{
 		Name:  "goport",
-		Usage: "Portscanner using go",
+		Usage: "goport -ip <ipv4> | <url> -p <22> | <22-80> | <22,80,443> | <-> for ports, if omitted 1-1024 are scanned --mode=<stealth> | <speed> | <accuracy> stealth is default",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
 				Name:  "p",
 				Value: "1-1024",
-				Usage: "-p 22,80 or -p 22 for specific ports // -p 22-443 for ranges // -p- for all ports // omitting -p or -p without any arguments scans the first 1024 ports",
+				Usage: "-p 22,80 or -p 22 for specific ports // -p 22-443 for ranges // -p - for all ports // omitting -p scans the first 1024 ports",
 			},
 			&cli.StringFlag{
 				Name:  "mode",
@@ -27,14 +31,38 @@ func main() {
 			},
 			&cli.StringFlag{
 				Name:  "ip",
-				Value: "0",
+				Value: "",
 				Usage: "target",
 			},
 		},
 		Action: func(cCtx *cli.Context) error {
-			handlePorts(cCtx.String("p"))
-			handleModes(cCtx.String("mode"))
-			handleTarget(cCtx.String("ip"))
+			var target string
+			var ports []int
+			var mode string
+			if cCtx.String("ip") == "" {
+				return cli.Exit("Target must be specified!", 1)
+			} else {
+				parsedTarget, errTargetHandler := handleTarget(cCtx.String("ip"))
+				if errTargetHandler != nil {
+					log.Fatalf("Error handling target: %v", errTargetHandler)
+				} else {
+					target = parsedTarget
+				}
+			}
+			parsedPorts, errPortHandler := handlePorts(cCtx.String("p"))
+			if errPortHandler != nil {
+				log.Fatalf("Error handling ports: %v", errPortHandler)
+			} else {
+				ports = parsedPorts
+			}
+
+			parsedMode, errModeHandler := handleModes(cCtx.String("mode"))
+			if errModeHandler != nil {
+				log.Fatalf("Error handling modes: %v", errModeHandler)
+			} else {
+				mode = parsedMode
+			}
+			scn.Scan(target, ports, mode)
 			return nil
 		},
 	}
@@ -43,16 +71,14 @@ func main() {
 	if err != nil {
 		log.Fatalf("Problem running arguments: %v\nos.Args: %v\n", err, os.Args)
 	}
-
+	fmt.Println("Time taken since start: ", time.Since(start))
 }
 
 // parseSinglePort extracts the port from the string, checks for errors, if none it returns the port as in
 func parseSinglePort(p string) (int, error) {
-	fmt.Printf("parsingSinglePort: %v\n", p)
 	port, err := strconv.Atoi(p)
 	if err != nil {
-		log.Fatalf("invalid port format: %v\terror: %v\n", port, err)
-		return 0, err
+		return 0, fmt.Errorf("<parseSinglePort> invalid port format: %v\terror: %v\n", p, err)
 	}
 	return port, nil
 }
@@ -67,8 +93,7 @@ func parseMultiPort(p string) ([]int, error) {
 		port, err := parseSinglePort(i)
 
 		if err != nil {
-			log.Fatalf("invalid port format: %v\terror: %v", p, err)
-			return nil, err
+			return nil, fmt.Errorf("<parseMultiPort> invalid port format: %v\terror: %v\n", p, err)
 		}
 		ports = append(ports, port)
 	}
@@ -84,7 +109,7 @@ func parsePortRange(p string) ([]int, error) {
 
 	if err1 != nil || err2 != nil {
 
-		return nil, fmt.Errorf("invalid port format: %v\t%v\terrors: %v\t%v\n", lower, upper, err1, err2)
+		return nil, fmt.Errorf("<parsePortRange> invalid port format: %v\terrors: %v\t%v\n", p, err1, err2)
 	}
 
 	var ports []int
@@ -92,44 +117,45 @@ func parsePortRange(p string) ([]int, error) {
 	for i := lower; i <= upper; i++ {
 		ports = append(ports, i)
 	}
-
-	fmt.Printf("parsingPortRange: %v\n", ports)
 	return ports, nil
 }
 
 // handlePorts is the wrapper function for the different inputs for the -p flag
 func handlePorts(p string) ([]int, error) {
-	fmt.Printf("handlePorts: %v\n", p)
 	var ports []int
 	// All ports or default case
 	if p == "1-1024" {
-		ports, err := parsePortRange(p)
+		parsedPorts, err := parsePortRange(p)
+		ports = append(ports, parsedPorts...)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("<handlePorts> error during default case: %v\terror: %v\n", p, err)
 		}
 		return ports, nil
 	} else if p == "-" {
-		ports, err := parsePortRange("1-65535")
+		parsedPorts, err := parsePortRange("1-65535")
+		ports = append(ports, parsedPorts...)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("<handlePorts> error all ports case: %v\terror: %v\n", p, err)
 		}
 		return ports, nil
 	} else if strings.Contains(p, "-") {
-		ports, err := parsePortRange(p)
+		parsedPorts, err := parsePortRange(p)
+		ports = append(ports, parsedPorts...)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("<handlePorts> error during range case: %v\terror: %v\n", p, err)
 		}
 		return ports, nil
 	} else if strings.Contains(p, ",") {
-		ports, err := parseMultiPort(p)
+		parsedPorts, err := parseMultiPort(p)
+		ports = append(ports, parsedPorts...)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("<handlePorts> error during multiple ports case: %v\terror: %v\n", p, err)
 		}
 		return ports, nil
 	} else {
 		single, err := parseSinglePort(p)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("<handlePorts> error during single port case: %v\terror: %v\n", p, err)
 		}
 		ports = append(ports, single)
 		return ports, nil
@@ -137,13 +163,19 @@ func handlePorts(p string) ([]int, error) {
 }
 
 // handleModes handles modes...
-func handleModes(m string) {
+func handleModes(m string) (string, error) {
 	fmt.Printf("handleModes: %v\n", m)
+	return "", nil
 
 }
 
 // handleTarget handles the target, if a URL is passed, the URL is resolved into an IPv4 address
-func handleTarget(t string) {
+func handleTarget(t string) (string, error) {
 	fmt.Printf("handleTarget: %v\n", t)
+	return "", nil
+}
 
+func resolveDomain(d string) (string, error) {
+	var ip string
+	return ip, nil
 }
--- pkg/scanner/scanner.go
+++ pkg/scanner/scanner.go
@@ -1,1 +1,7 @@
 package scanner
+
+import "fmt"
+
+func Scan(t string, p []int, m string) {
+	fmt.Printf("Target: %v\nPorts: %v\nMode: %v", t, p, m)
+}
