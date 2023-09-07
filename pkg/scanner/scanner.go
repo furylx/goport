b172b694f209d49ef461271dae2af9a44b8ddd9c
furylx <101798731+furylx@users.noreply.github.com>
2023-09-07 20:58:00 +0200

    bla

--- pkg/scanner/scanner.go
+++ pkg/scanner/scanner.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"sync"
 	"time"
 
 	"github.com/google/gopacket/pcap"
@@ -11,11 +12,23 @@ import (
 
 var (
 	listener ScanListener
+	wg       sync.WaitGroup
 )
+
+// func getLocalIp(i string) []net.Addr {
+// 	localIface, err := net.InterfaceByName(i)
+// 	localIp, err := localIface.Addrs()
+// 	if err != nil {
+// 		log.Fatalf("<getLocalIp> Could not retrieve ip address of the specified interface: %v\t error: %v\n", i, err)
+// 	}
+// 	fmt.Println("<getLocalIp>: ", localIp)
+// 	return localIp
+// }
 
 // Scan creates the handle to send and receive the packets, initiates the right scan mode and closes the handle and the listener when done
 func Scan(t net.IP, p []int, m string, iface string) {
 	fmt.Printf("Target: %v\nPorts: %v\nMode: %v\nInterface: %v\n", t, p, m, iface)
+
 	// creating channel to close the listener
 	stopCh := make(chan bool)
 	// openhandle
@@ -37,10 +50,11 @@ func Scan(t net.IP, p []int, m string, iface string) {
 			iface: iface,
 			mode:  m,
 		}
-		go listener.Start(m, iface, handle, stopCh)
+		wg.Add(1)
+		go listener.Start(iface, m, handle, stopCh, t)
 		// loop over ports and send packets via handle
 		InitiateStealthScan(t, p, handle)
-
+		wg.Wait()
 	case "speed":
 		// start listener (pass mode into listener) in goroutine
 		// loop over ports and send packets via handle
@@ -50,15 +64,16 @@ func Scan(t net.IP, p []int, m string, iface string) {
 		// loop over ports and send packets via handle
 
 	}
-
-	time.Sleep(10)
+	fmt.Println("#########sleeping 10#########")
+	time.Sleep(time.Second * 10)
 
 	// stop listener
+	fmt.Println("calling stop")
 	listener.Stop(stopCh)
 }
 
 // ScanListener serves as interface to end all the different listeners depending on the mode
 type ScanListener interface {
-	Start(i string, m string, h *pcap.Handle, c chan bool)
+	Start(i string, m string, h *pcap.Handle, c chan bool, t net.IP)
 	Stop(c chan bool)
 }
--- pkg/scanner/stealth.go
+++ pkg/scanner/stealth.go
@@ -1,8 +1,11 @@
 package scanner
 
 import (
+	"fmt"
 	"log"
+	"math/rand"
 	"net"
+	"time"
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
@@ -18,12 +21,18 @@ type StealthListener struct {
 	mode  string
 }
 
-func (s *StealthListener) Start(i string, m string, h *pcap.Handle, c chan bool) {
+func (s *StealthListener) Start(i string, m string, h *pcap.Handle, c chan bool, t net.IP) {
+	defer wg.Done()
 	packetSource := gopacket.NewPacketSource(h, h.LinkType())
+	fmt.Println("Stealth listener starting...", packetSource.Packets())
+	time.Sleep(time.Second * 5)
 	for {
 		select {
 		case packet := <-packetSource.Packets():
-			handleResponse(packet, m)
+			if packet.Layer(layers.LayerTypeIPv4) != nil && packet.Layer(layers.LayerTypeTCP) != nil {
+				handleResponse(packet, m, t)
+			}
+
 		case <-c:
 			return
 		}
@@ -31,15 +40,21 @@ func (s *StealthListener) Start(i string, m string, h *pcap.Handle, c chan bool)
 }
 
 func (s *StealthListener) Stop(c chan bool) {
+	fmt.Println("Killing listener")
 	c <- false
 }
 
 func InitiateStealthScan(t net.IP, p []int, h *pcap.Handle) {
+	fmt.Println("Initiating stealth scan...")
 	for _, port := range p {
-		err := h.WritePacketData(craftSynPacket(t, port))
-		if err != nil {
-			log.Fatalf("<InitiateStealthScan> error sending out packet: %v\n", err)
-		}
+		packet := craftSynPacket(t, port)
+		fmt.Println("THE PACKET: ", packet)
+		testing(packet)
+		// err := h.WritePacketData(packet)
+		// fmt.Println("Packets out!! ", err)
+		// if err != nil {
+		// 	log.Fatalf("<InitiateStealthScan> error sending out packet: %v\n", err)
+		// }
 	}
 }
 
@@ -47,34 +62,55 @@ func craftSynPacket(t net.IP, p int) []byte {
 	buf := gopacket.NewSerializeBuffer()
 	opts := gopacket.SerializeOptions{}
 	gopacket.SerializeLayers(buf, opts,
-		&layers.Ethernet{},
 		&layers.IPv4{
-			DstIP: t,
+			DstIP:    t,
+			SrcIP:    net.IPv4(10, 1, 1, 27),
+			Protocol: layers.IPProtocolTCP,
 		},
 		&layers.TCP{
 			DstPort: layers.TCPPort(p),
+			SrcPort: layers.TCPPort(35000),
 			SYN:     true,
+			Seq:     uint32(rand.Int31()),
 		})
 	packetData := buf.Bytes()
+	fmt.Println("Crafting packet: ", packetData)
 	return packetData
 }
 
 // func sendPacket(t net.IP, p []byte, iface string) error {}
 
-func handleResponse(p gopacket.Packet, m string) map[int]string {
+func handleResponse(p gopacket.Packet, m string, t net.IP) map[int]string {
 	// out := make(map[int]string)
 	switch m {
 	case "stealth":
 		ipv4Layer := p.Layer(layers.LayerTypeIPv4)
 		if ipv4Layer != nil {
 			ipv4, _ := ipv4Layer.(*layers.IPv4) // Type assertion to get the actual IPv4 layer type
+			if ipv4.DstIP.Equal(t) {
+				fmt.Printf("<handleResponse ipv4: %v\t%v\t%v>\n", ipv4.SrcIP, ipv4.DstIP, t)
+			}
+			fmt.Printf("%v\t%v\n", ipv4.DstIP, ipv4.SrcIP)
 		}
-		tcpLayer := p.Layer(layers.LayerTypeTCP)
-		if tcpLayer != nil {
-			tcp, _ := tcpLayer.(*layers.TCP)
-		}
+		// tcpLayer := p.Layer(layers.LayerTypeTCP)
+		// if tcpLayer != nil {
+		// 	tcp, _ := tcpLayer.(*layers.TCP)
+		// 	fmt.Printf("<handleResponse : %v>\n", tcp)
+		// }
 		// check for response according to stealth scan
 	}
 
 	return nil
+}
+
+func testing(p []byte) {
+	conn, err := net.Dial("ip4:tcp", "10.1.1.35")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = conn.Write(p)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
